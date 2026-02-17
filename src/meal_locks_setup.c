1188fc7e93174e1d5285d330b96db11ffaa4d77f
omaly <ohnivasasanka@gmail.com>
2026-02-17 10:19:04 +0100

    double free fix

--- src/meal_locks_setup.c
+++ src/meal_locks_setup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   meal_locks_setup.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omaly <omaly@student.42.fr>                +#+  +:+       +#+        */
+/*   By: omaly <student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 16:56:52 by omaly             #+#    #+#             */
-/*   Updated: 2026/01/21 15:25:45 by omaly            ###   ########.fr       */
+/*   Updated: 2026/02/17 10:18:51 by omaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,19 @@ int	meal_locks_setup(pthread_mutex_t **meal_locks, size_t philo_count)
 
 	*meal_locks = malloc(philo_count * sizeof(pthread_mutex_t));
 	if (*meal_locks == NULL)
-	{
 		return (print_error(ERR_MALLOC));
-	}
 	i = 0;
 	while (i < philo_count)
 	{
 		if (pthread_mutex_init(&(*meal_locks)[i], NULL) != 0)
 		{
+			while (i > 0)
+			{
+				i--;
+				pthread_mutex_destroy(&(*meal_locks)[i]);
+			}
 			free(*meal_locks);
+			*meal_locks = NULL;
 			return (print_error(ERR_MUTEX_INIT));
 		}
 		i++;
